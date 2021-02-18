2e1251c2f92c441417e295d3ba54f43933a5951b
mzaja <64711255+mzaja@users.noreply.github.com>
2021-02-18 22:13:53 +0000

    Implemented Element class, text search and waits

--- selex.py
+++ selex.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, WebDriverException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -8,7 +9,7 @@ SUPPORTED_BROWSERS = ['Chrome', 'Firefox', 'Ie', 'Edge']
 
 class Driver(webdriver.Chrome, webdriver.Firefox, webdriver.Ie, webdriver.Edge):
     """
-    doc
+    A custom Selenium webdriver with extended functionality.
     """
     def __init__(self, browser: str):
         browser = browser.capitalize()
@@ -17,7 +18,8 @@ class Driver(webdriver.Chrome, webdriver.Firefox, webdriver.Ie, webdriver.Edge):
         else:
             getattr(webdriver, browser).__init__(self)
         
-        self.press = KeyPress(self)
+        self._web_element_cls = Element     # return custom WebElement class using this webdriver
+        self.press = DriverKeyPress(self)
         self._implicit_wait = 0
     
     @property
@@ -31,14 +33,49 @@ class Driver(webdriver.Chrome, webdriver.Firefox, webdriver.Ie, webdriver.Edge):
         self.implicitly_wait(value)
         self._implicit_wait = value
 
+    def find_element_by_text(self, text: str, exact_match: bool = False):
+        """Finds and returns an element by its text value."""
+        return find_elements_by_text(self, True, text, exact_match)
+    
+    def find_elements_by_text(self, text: str, exact_match: bool = False):
+        """Finds and returns elements by their text value."""
+        return find_elements_by_text(self, False, text, exact_match)
+    
 
-class KeyPress:
+class Element(WebElement):
     """
-    doc
+    A custom WebElement class returned by the custom Selenium webdriver.
+    """
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.press = ElemKeyPress(self)
+    
+    def find_element_by_text(self, text: str, exact_match: bool = False):
+        """Finds and returns an element by its text value."""
+        return find_elements_by_text(self._parent, True, text, exact_match)
+    
+    def find_elements_by_text(self, text: str, exact_match: bool = False):
+        """Finds and returns elements by their text value."""
+        return find_elements_by_text(self._parent, False, text, exact_match)
+
+
+class BaseKeyPress:
+    """
+    Base KeyPress class. 
+    Allows creation of two separate KeyPress classes, one for the driver and ther other for web elements.
+    """
+    def __init__(self):
+        self.keys = tuple(x for x in dir(Keys) if not x.startswith('__'))
+
+
+class DriverKeyPress(BaseKeyPress):
+    """
+    Simulates key presses in a simple and convenient way. 
     """
     def __init__(self, driver):
+        super().__init__()
         self._driver = driver
-        self.keys = tuple(x for x in dir(Keys) if not x.startswith('__'))
         for key_name in self.keys:
             # have key press available as attribute (property) e.g. press.ENTER
             #setattr(self.__class__, key_name, property(fget=lambda self, x=getattr(Keys, key_name): self._press_key(x)))
@@ -50,24 +87,84 @@ class KeyPress:
         action = ActionChains(self._driver)
         action.send_keys(key)
         action.perform()
-        
-        
-def wait(enable: bool = True):      # work in progress -> create a @wait(t) decorator
+
+
+class ElemKeyPress(BaseKeyPress):
     """
-    Decorator with parameter 'enable': 
-        If True: Apply implicit wait to the nominated class method. 
-        if False: Do not apply implicit wait to the nominated class method.
-    
-    Selenium webdriver must be available as an attribute at 'self._driver'.
-    Default implicit wait must be attribute at 'self._implicit_wait'.
+    Simulates key presses in a simple and convenient way. 
+    """
+    def __init__(self, element):
+        super().__init__()
+        self._element = element
+        for key_name in self.keys:
+            setattr(self, key_name, lambda x=getattr(Keys, key_name): self._element.send_keys(x))
+            
+
+def find_elements_by_text(driver: Driver, return_one: bool, text: str, exact_match: bool):
+    """Finds element(s) by their text. A base function for Driver and Element class methods"""
+    contains_query = f"//*[contains(text(), '{text}')]"
+    exact_query = f"//*[text()='{text}']"
+    query = exact_query if exact_match == True else contains_query
+    method = driver.find_element_by_xpath if return_one == True else driver.find_elements_by_xpath
+    return method(query)
+
+
+def wait(time: float): 
+    """
+    Intended to be used as a decorator within the Driver class. 
+    Forces the implicit wait on executing the class methods.
+    Example:
+        @wait(5)
+        def method(self, *args, **kwargs)
+        ->
+        method is executed with an implicit webdriver wait of 5 seconds.
     """
     def decorator(method):
         def wrapper(self, *args, **kwargs):
-            default_wait = self._implicit_wait
-            temporary_wait = IMPLICIT_WAIT if enable == True else 0
-            self._driver.implicitly_wait(temporary_wait)
-            retval = method(self, *args, **kwargs)
-            self._driver.implicitly_wait(default_wait)
-            return retval
+            old_wait = self.implicit_wait
+            self.implicit_wait = new_wait
+            try:
+                return method(self, *args, **kwargs)
+            finally:
+                self.implicit_wait = old_wait
         return wrapper
     return decorator
+
+
+def wait_factory(driver_attr_name: str):
+    """
+    Returns a wait decorator for a webdriver class atribute named driver_attr_name.
+    Example:
+        wait = wait_factory("driver")
+        
+        class HasDriverAsAttribute:
+            def __init__(self):
+                self.driver = Driver("Chrome")
+            
+            @wait(5)
+            def method(self, *args, **kwargs):
+                <Does something with self.driver>
+        
+        -> method is executed with an implicit webdriver wait of 5 seconds
+    """
+    def wait(time: float): 
+        """
+        Intended to be used as a decorator within the Driver class. 
+        Forces the implicit wait on executing the class methods.
+        Example:
+            @wait(5)
+            def method(self, *args, **kwargs)
+            ->
+            method is executed with an implicit webdriver wait of 5 seconds.
+        """
+        def decorator(method):
+            def wrapper(self, *args, **kwargs):
+                old_wait = getattr(self, driver_attr_name).implicit_wait
+                getattr(self, driver_attr_name).implicit_wait = new_wait
+                try:
+                    return method(self, *args, **kwargs)
+                finally:
+                    getattr(self, driver_attr_name).implicit_wait = old_wait
+            return wrapper
+        return decorator
+    return wait
--- workbench.py
+++ workbench.py
@@ -3,6 +3,31 @@ from selex import *
 # initialize for testing
 driver = Driver('Chrome')
 driver.get('https://www.google.com')
+driver.implicit_wait
+driver.find_element_by_text('I agree')
+driver.implicit_wait
+driver.implicit_wait = 5
+driver.implicit_wait = 0
+driver.find_elements_by_text('I agree')
+
+e = driver.find_element_by_css_selector("input[title=Search]")
+e.send_keys("Python")
+e.press.ENTER()
+
+e = driver.find_element_by_text('About')
+e.find_element_by_text('About').click()
+f = driver.find_element_by_css_selector("div")
+
+
+# test finding elements by text
+driver.find_element_by_text('I agree').click()
+driver.find_element_by_text('See more')
+driver.find_element_by_text('About')
+driver.find_element_by_text('OK').click()
+driver.find_element_by_text('learn', True).click()
+
+driver._web_element_cls
+driver.create_web_element
 
 # key press test
 driver.press.keys
@@ -17,6 +42,8 @@ driver.implicit_wait = 0
 driver.implicit_wait
 driver.find_element_by_id("surely no such element exists")      # takes 0 seconds
 
+driver.find_element_by_id()
+driver.find_element_by_css_selector()
     
 if __name__ == "__main__":
     for browser in SUPPORTED_BROWSERS:
