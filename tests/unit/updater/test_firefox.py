90a436f39cbcbb724a29fbb66c6d594b5065e7c8
mzaja <64711255+mzaja@users.noreply.github.com>
2021-08-05 23:09:21 +0100

    Firefox updater tests added

--- tests/unit/updater/test_firefox.py
+++ tests/unit/updater/test_firefox.py
@@ -1,6 +1,7 @@
+import io
 import unittest
 from pathlib import Path
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 import selex.updater.firefox    # must import like this before "from A import B", else function B is not patchable
 from selex.updater.firefox import (get_firefox_bit_version_win, 
@@ -70,6 +71,85 @@ of the license at https://mozilla.org/MPL/2.0/.\n"""
         mock_check_output.return_value = self.SHELL_RAW_OUTPUT
         self.assertEqual(self.VERSION, get_geckodriver_version_win())
         mock_locate_geckodriver.assert_called_once()
+
+
+@patch("requests.get")
+class GetLatestGeckoDriverVersionTest(unittest.TestCase):
+    """
+    Tests the 'get_latest_geckodriver_version' function.
+    
+    Uses a real website snapshot.
+    """
+    def test_(self, mock_requests_get):
+        VERSION = "0.29.1"
+        mock_website_path = Path("tests/resources/geckodriver_downloads_page.html")
+        with io.open(mock_website_path, "r",encoding="utf-8") as mock_website:
+            mock_requests_get.return_value.text = mock_website
+            self.assertEqual(VERSION, get_latest_geckodriver_version())
+
+
+@patch("builtins.print")
+@patch("selex.updater.firefox.zip_download_and_extract")
+@patch("selex.updater.firefox.get_firefox_bit_version_win")
+@patch("selex.updater.firefox.get_latest_geckodriver_version")
+@patch("selex.updater.firefox.get_geckodriver_version_win")
+@patch("selex.updater.firefox.locate_geckodriver")
+class UpdateGeckoDriverTest(unittest.TestCase):
+    """
+    Tests the 'update_geckodriver' function.
+    """
+    CURRENT_VERSION = "0.29.1"
+    
+    def test_newer_exists(self, 
+                          mock_locate_geckodriver, 
+                          mock_get_geckodriver_version_win,
+                          mock_get_latest_geckodriver_version,
+                          mock_get_firefox_bit_version_win,
+                          mock_zip_download_and_extract,
+                          mock_print):
+        mock_get_geckodriver_version_win.return_value = self.CURRENT_VERSION
+        mock_get_latest_geckodriver_version.return_value = "0.29.2"  
+        update_geckodriver(force = False)
+        mock_locate_geckodriver.assert_called_once()
+        mock_get_geckodriver_version_win.assert_called_once()
+        mock_get_latest_geckodriver_version.assert_called_once()
+        mock_get_firefox_bit_version_win.assert_called_once()
+        mock_zip_download_and_extract.assert_called_once()
+        self.assertEqual(mock_print.call_count, 4)
+
+    def test_newer_does_not_exist(self, 
+                                  mock_locate_geckodriver, 
+                                  mock_get_geckodriver_version_win,
+                                  mock_get_latest_geckodriver_version,
+                                  mock_get_firefox_bit_version_win,
+                                  mock_zip_download_and_extract,
+                                  mock_print):
+        mock_get_geckodriver_version_win.return_value = self.CURRENT_VERSION
+        mock_get_latest_geckodriver_version.return_value = self.CURRENT_VERSION  
+        update_geckodriver(force = False)
+        mock_locate_geckodriver.assert_called_once()
+        mock_get_geckodriver_version_win.assert_called_once()
+        mock_get_latest_geckodriver_version.assert_called_once()
+        mock_get_firefox_bit_version_win.assert_not_called()
+        mock_zip_download_and_extract.assert_not_called()
+        self.assertEqual(mock_print.call_count, 3)
+
+    def test_newer_does_not_exist_force(self, 
+                                        mock_locate_geckodriver, 
+                                        mock_get_geckodriver_version_win,
+                                        mock_get_latest_geckodriver_version,
+                                        mock_get_firefox_bit_version_win,
+                                        mock_zip_download_and_extract,
+                                        mock_print):
+        mock_get_geckodriver_version_win.return_value = self.CURRENT_VERSION
+        mock_get_latest_geckodriver_version.return_value = self.CURRENT_VERSION  
+        update_geckodriver(force = True)
+        mock_locate_geckodriver.assert_called_once()    # same calls as for when a new version exists
+        mock_get_geckodriver_version_win.assert_called_once()
+        mock_get_latest_geckodriver_version.assert_called_once()
+        mock_get_firefox_bit_version_win.assert_called_once()
+        mock_zip_download_and_extract.assert_called_once()
+        self.assertEqual(mock_print.call_count, 4)
 
 
 if __name__ == "__main__":
