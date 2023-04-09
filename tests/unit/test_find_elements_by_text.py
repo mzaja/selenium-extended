c24dd3da2421d8d196fd53b2eab99b1b076f23ed
mzaja <64711255+mzaja@users.noreply.github.com>
2023-04-09 21:28:51 +0100

    Update tests for 100% coverage

--- tests/unit/test_find_elements_by_text.py
+++ tests/unit/test_find_elements_by_text.py
@@ -1,80 +1,62 @@
 import unittest
 
 from tests.setup import BaseTestCase
-from selex import *
-
-
-def single_no_match(self, driver):
-    test_phrase = "Nonexistent text"
-    with self.assertRaises(NoSuchElementException):
-        elem = driver.find_element(By.PARTIAL_TEXT, test_phrase)      
-
-def single_match(self, driver):
-    test_phrase = "Heading"
-    elem = driver.find_element(By.PARTIAL_TEXT, test_phrase)
-    self.assertIn(test_phrase, elem.text)
-
-def single_no_match_exact(self, driver):
-    test_phrase = "Heading"
-    with self.assertRaises(NoSuchElementException):
-        elem = driver.find_element(By.TEXT, test_phrase)      
-
-def single_match_exact(self, driver):
-    test_phrase = "Heading 1"
-    elem = driver.find_element(By.TEXT, test_phrase)
-    self.assertEqual(elem.text, test_phrase)
-
-
-def multi_no_match(self, driver):
-    test_phrase = "Nonexistent text"   
-    elems = driver.find_elements(By.PARTIAL_TEXT, test_phrase)
-    self.assertEqual(len(elems), 0)
-
-def multi_match(self, driver):
-    test_phrase = "Heading"
-    elems = driver.find_elements(By.PARTIAL_TEXT, test_phrase)
-    self.assertEqual(len(elems), 3)
-    for elem in elems:
-        self.assertIn(test_phrase, elem.text)
-
-def multi_no_match_exact(self, driver):
-    test_phrase = "Heading"
-    elems = driver.find_elements(By.TEXT, test_phrase)
-    self.assertEqual(len(elems), 0)
-
-def multi_match_exact(self, driver):
-    test_phrase = "Heading 1"
-    elems = driver.find_elements(By.TEXT, test_phrase)
-    self.assertEqual(len(elems), 1)
-    for elem in elems:
-        self.assertEqual(test_phrase, elem.text)
+from selex import NoSuchElementException, By
 
 
 class BaseFindElementsByTextTest:
+    """
+    Base test case for locating elements by text,
+    """
     
     def test_single_no_match(self):
-        single_no_match(self, self.subject)
+        test_phrase = "Nonexistent text"
+        with self.assertRaises(NoSuchElementException):
+            self.subject.find_element(By.PARTIAL_TEXT, test_phrase)  
     
     def test_single_match(self):
-        single_match(self, self.subject)
+        test_phrase = "Heading"
+        elem = self.subject.find_element(By.PARTIAL_TEXT, test_phrase)
+        self.assertIn(test_phrase, elem.text)
     
     def test_single_no_match_exact(self):
-        single_no_match(self, self.subject)
+        test_phrase = "Heading"
+        with self.assertRaises(NoSuchElementException):
+            self.subject.find_element(By.TEXT, test_phrase)     
     
     def test_single_match_exact(self):
-        single_match(self, self.subject)
+        test_phrase = "Heading 1"
+        elem = self.subject.find_element(By.TEXT, test_phrase)
+        self.assertEqual(elem.text, test_phrase)
 
     def test_multi_no_match(self):
-        multi_no_match(self, self.subject)
+        test_phrase = "Nonexistent text"   
+        elems = self.subject.find_elements(By.PARTIAL_TEXT, test_phrase)
+        self.assertEqual(len(elems), 0)
     
     def test_multi_match(self):
-        multi_match(self, self.subject)
+        test_phrase = "Heading"
+        elems = self.subject.find_elements(By.PARTIAL_TEXT, test_phrase)
+        self.assertEqual(len(elems), 3)
+        for elem in elems:
+            self.assertIn(test_phrase, elem.text)
     
     def test_multi_no_match_exact(self):
-        multi_no_match(self, self.subject)
+        test_phrase = "Heading"
+        elems = self.subject.find_elements(By.TEXT, test_phrase)
+        self.assertEqual(len(elems), 0)
     
     def test_multi_match_exact(self):
-        multi_match(self, self.subject)
+        test_phrase = "Heading 1"
+        elems = self.subject.find_elements(By.TEXT, test_phrase)
+        self.assertEqual(len(elems), 1)
+        for elem in elems:
+            self.assertEqual(test_phrase, elem.text)
+    
+    def test_non_text(self):
+        """Required to attain 100 % coverage."""
+        self.subject.find_elements(By.CSS_SELECTOR, "body")
+        # If it does not crash, it passes
 
 
 class DriverFindElementsByTextTest(BaseTestCase, BaseFindElementsByTextTest):
